09c519e07911c7cd54ff194077e858f990a35f35
Aakash Patel <akashpatelp83@gmail.com>
2023-07-18 01:12:38 +0530

    Challenge 1 mySoluton
    - Your Initials printing on console.

---
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitialsMySolution.java
@@ -0,0 +1,12 @@
+public class YourInitialsMySolution {
+    public static void main(String[] args) {
+         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+        System.out.println("  A    PPPP");
+        System.out.println(" A A   P   P");
+        System.out.println("A   A  P   P");
+        System.out.println("AAAAA  PPPP");
+        System.out.println("A   A  P   ");
+        System.out.println("A   A  P   ");
+        System.out.println("A   A  P   ");
+    }
+}
