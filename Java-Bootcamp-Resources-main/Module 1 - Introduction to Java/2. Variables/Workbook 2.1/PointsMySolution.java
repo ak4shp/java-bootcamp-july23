ee4af0b92b55ffa2446ad7a23b973134b1e43b90
Aakash Patel <akashpatelp83@gmail.com>
2023-07-18 10:32:54 +0530

    Workbook 2.1 mySolution

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.1/PointsMySolution.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.1/PointsMySolution.java
@@ -1,4 +1,4 @@
-public class Points {
+public class PointsMySolution {
     public static void main(String[] args) {
         
         // Instructions for this workbook are on Learn the Part (see the Udemy Video: Workbook 2.1 to access the link).
@@ -7,17 +7,24 @@ public class Points {
 
         System.out.println("Harry was caught wandering the halls. -50 points for Gryffindor");
         //update points here
+        points -= 50;
         System.out.println("Harry was being cheeky in class. -3 points for Gryffindor");
         //update points here
+        points -= 3;
         System.out.println("Hermione got full marks on Lockhart's quiz. 30 points for Gryffindor");
         //update points here
+        points += 30;
         System.out.println("Ron won the underground chess game. 100 points for Gryffindor");
         //update points here
+        points += 100;
+
         System.out.println("Harry defeated Quirrell. 60 points for Gryffindor");
-        //update points here        
-        
-        // Compare your result to what's on Learn the Part.
+        //update points here   
+        points += 60;
+            
         System.out.println(points);
+        // Compare your result to what's on Learn the Part.
+        
     }
 }
 
