56633d97978b461cbd6ff05bade2e77c71a35fe7
Aakash Patel <akashpatelp83@gmail.com>
2023-07-20 00:28:24 +0530

    Workbook 2.3 mySolution

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.3/AppleStoreMySolution.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.3/AppleStoreMySolution.java
@@ -1,4 +1,4 @@
-public class AppleStore {
+public class AppleStoreMySolution {
     public static void main(String[] args) {
         int numOfApples = 0;
         int numOfCustomers = 0;
@@ -7,22 +7,31 @@ public class AppleStore {
         // Instructions for this workbook are on Learn the Part (see the Udemy Video: Workbook 2.3 to access the link).
 
         System.out.println("You picked 500 apples from an apple orchard");
-        
+        numOfApples = 500;
+
         System.out.println("Time for business! You're selling each apple for 40 cents");
+        int price = 40;
 
         System.out.println("One customer walked in. He bought 4 apples!");
+        numOfCustomers += 1;
+        numOfApples -= 4;
+        profit +=  4 * price;
 
         System.out.println("Another customer walked in. He bought 20 apples!");
+        numOfCustomers += 1;
+        numOfApples -= 20;
+        profit += 20 * price;
 
         System.out.println("Another customer walked in. She bought 200 apples!");
-        
+        numOfCustomers += 1;
+        numOfApples -= 200;
+        profit += 200 * price;
 
-        System.out.println("Wow! So far, you made: $<profit>");
-        System.out.println("<number of customers> + customers love your apples.");
-        System.out.println("You have <number of apples> apples left.");
+        System.out.println("Wow! So far, you made: $"+ profit/100); // 100 cents equals one doller
+        System.out.println(numOfCustomers +" customers love your apples.");
+        System.out.println("You have "+ numOfApples+" apples left.");
 
-        // Compare your result to what's on Learn the Part.  
-        System.out.println(numOfApples + numOfCustomers + profit);
+        // Compare your result to what's on Learn the Part.
     }
 
 }
