41a22af0c86081d48f167d87c08d3fec5de840a9
Aakash Patel <akashpatelp83@gmail.com>
2023-07-19 23:59:30 +0530

    Workbook 2.1 mySolution

---
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.2/BiographyMySolution.java
@@ -0,0 +1,22 @@
+public class BiographyMySolution {
+    public static void main(String[] args) {
+        
+        // Instructions for this workbook are on Learn the Part (see the Udemy Video: Workbook 2.2 to access the link).
+
+        String name = "Frinds";
+        int age = 25;
+        String country = "India";
+        String sport = "Chess";
+        int hours = 2;
+        String game = "Mind Catch";
+        String subject = "Psychology";
+        Character grade = 'R';
+     
+        System.out.println("My name is "+name+ ". I'm "+age+" years old, and I'm from "+country);
+        System.out.println("My favourite sport is "+sport+". I play for "+hours+" hours a day");
+        System.out.println("When I'm tired, I like to play "+game+".");
+        System.out.println("In school, my favourite subject was "+subject+", I scored an "+grade+".");
+
+        // Compare your result to what's on Learn the Part.
+    }
+}
