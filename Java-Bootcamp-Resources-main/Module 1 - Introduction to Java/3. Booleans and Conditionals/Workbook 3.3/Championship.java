57cfd11a896b6ab0884a303d3af507cfd8691474
Aakash Patel <akashpatelp83@gmail.com>
2023-07-20 23:14:02 +0530

    Workbook 3.3 and 3.4

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.3/Championship.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.3/Championship.java
@@ -4,9 +4,17 @@ public class Championship {
         //See Learn the Part for the complete instructions (link in resources folder of Udemy video).  
 
         int gryffindor = 400;    //gryffindor points
-        int ravenclaw = 200;    //ravenclaw points
+        int ravenclaw = 490;    //ravenclaw points
 
-        // int margin = amount of points by which gryffindor scored over ravenclaw;
-        
+        int margin = gryffindor - ravenclaw;
+        if(margin >= 300){
+            System.out.println("Gryffindor takes the house cup!");
+        } else if(margin >= 0){
+            System.out.println("In second place, Gryffindor!");
+        } else if(margin >= -100){
+            System.out.println("In third place, Gryffindor!");
+        } else{
+            System.out.println("In fourth place, Gryffindor!");
+        }
     }
 }
--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.4/WeatherNetwork.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.4/WeatherNetwork.java
@@ -4,13 +4,19 @@ public class WeatherNetwork {
         //See Learn the Part for the complete instructions (link in resources folder of Udemy video).  
 
         
-        int temp = 25;  
+        int temp = 5;  
 
         String forecast = "";
 
         //IF - ELSE IF - ELSE STATEMENTS HERE!
-        
-        
+        if(temp <= -1){
+            forecast = "FREEZING! Stay home!";
+        } else if(temp <= 10){
+            forecast = "Chilly. Wear a coat!";
+        } else{
+            forecast = "It's warm. Go outside!";  
+        }
+
         System.out.println(forecast);
     }
 }
