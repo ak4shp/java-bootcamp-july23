4b6dedf2dd1142f26a3b94c0ef24634320e3c6a3
Aakash Patel <akashpatelp83@gmail.com>
2023-07-20 18:26:16 +0530

    Refactor: Challenge 2 Chatbot

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Challenge 2/Chatbot.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Challenge 2/Chatbot.java
@@ -13,25 +13,21 @@ public class Chatbot {
         System.out.println("Hello. What is your name?");
         //Pick up user's name and store it. 
         String name = scan.nextLine();
-        scan.nextLine();
                            
         //add new a line before asking next question. 
         System.out.println("Hi "+name+"! I'm Javabot. Where are you from?");
         //Pick up user's home and store it. 
         String home = scan.nextLine();
-        scan.nextLine();    
 
         //add new a line before asking next question.
-        System.out.println("I hear it's beautiful at "+home+"! I'm from a place called Oracle");
+        System.out.println("\nI hear it's beautiful at "+home+"! I'm from a place called Oracle");
         System.out.println("How old are you?");
         //Pick up age and store it.
         int age = scan.nextInt();
         scan.nextLine();
-        scan.nextLine();
-
         //add new a line before asking next question.
 
-        System.out.println("So you're "+age+", cool! I'm 400 years old.");
+        System.out.println("\nSo you're "+age+", cool! I'm 400 years old.");
         System.out.println("This means I'm "+(400/age)+" times older than you.");
         System.out.println("Enough about me. What's your favourite language? (just don't say Python)");
         //Pick up language and store it. 
