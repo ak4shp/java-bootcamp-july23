5721070eb04da6ab6ff46cb8488d94dd345dc591
Aakash Patel <akashpatelp83@gmail.com>
2023-07-20 23:47:50 +0530

    Logical Operators
    - Workbook 3.5

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.5/WorkSchedule.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.5/WorkSchedule.java
@@ -3,12 +3,18 @@ public class WorkSchedule {
         
         //See Learn the Part for the complete instructions (link in resources folder of Udemy video).  
 
-        int day = 3;   //3rd day of the week...
-        boolean holiday = true;
+        int day = 6;   //3rd day of the week...
+        boolean holiday = false;
          
         // IF - ELSE IF - ELSE HERE!
         
-        
+        if(holiday){
+            System.out.println("Woohoo, no work");
+        } else if(day == 6 || day == 7){
+            System.out.println("It's the weekend, no work!");
+        } else{
+            System.out.println("Wake up at 7:00 :(");
         }
-
     }
+
+}
--- Practice-Projects/LogicalOperators.java
+++ Practice-Projects/LogicalOperators.java
@@ -1,19 +1,26 @@
 public class LogicalOperators {
     public static void main(String[] args) {
 
-        /**********************AND OPERATOR**********************/
+        //**********************AND OPERATOR**********************/
 
         boolean hasDrivingLicense = true;
         boolean hasInsurance = true;
 
-        // has License and Insurance: System.out.println("You can legally drive.");
-        // otherwise: System.out.println("You cannot legally drive.");
+        if(hasDrivingLicense && hasInsurance){
+            System.out.println("You can legally drive.");
+        } else{
+            System.out.println("You cannot legally drive.");
+        }
+
 
         int age = 25;
         double income = 45000;
 
-        // is at least 21 and has an income of at least 40000 : System.out.println("You are eligible for a loan.");
-        // otherwise: System.out.println("You are not eligible for a loan.");
+        if(age >= 21 && income >= 40000){
+            System.out.println("You are eligible for a loan.");
+        } else{
+            System.out.println("You are not eligible for a loan.");
+        }
 
 
         String inputUsername = "JohnDoe";
@@ -22,52 +29,69 @@ public class LogicalOperators {
         String correctUsername = "JohnDoe";
         String correctPassword = "password123";
 
-        // the inputted username and password are correct: System.out.println("Access granted!");
-        // otherwise: System.out.println("Invalid username or password");
+        if(inputUsername.equals(correctUsername) && inputPassword.equals(correctPassword)){
+            System.out.println("Access granted!");
+        } else{
+            System.out.println("Invalid username or password");
+        }
 
-        /**********************OR OPERATOR**********************/
+
+
+        //**********************OR OPERATOR**********************/
 
         boolean hasGoodPerformance = true;
         boolean isLongTermEmployee = false;
+        if(hasGoodPerformance || isLongTermEmployee){
+            System.out.println("The user is eligible for a promotion.");
+        } else{
+            System.out.println("The user is not eligible for a promotion.");
+        }
 
-        // good performance or is a long term employee: System.out.println("The user is eligible for a promotion.");
-        // otherwise: System.out.println("The user is not eligible for a promotion.");
 
         int userAge = 17;
         boolean isParentPresent = true;
-
-        // at least 18 or if parent is present: System.out.println("You can enter the venue.");
-        // otherwise: System.out.println("You cannot enter the venue.");
+        if(userAge >= 18 || isParentPresent){
+            System.out.println("You can enter the venue.");
+        } else{
+            System.out.println("You cannot enter the venue.");
+        }
 
 
         int memberAge = 16;
         boolean hasMembership = false;
+        if(memberAge >= 18 || hasMembership){
+            System.out.println("The user can access the service.");
+        } else{
+            System.out.println("The user cannot access the service.");
+        }
 
-        // at least 18 or has a membership: System.out.println("The user can access the service.");
-        // otherwise: System.out.println("The user cannot access the service.");
 
-
-        /**********************NOT OPERATOR**********************/
+        //**********************NOT OPERATOR**********************/
 
         String option = "cash";
-
         boolean cashOrCredit = option.equals("cash") || option.equals("credit");
+        if(!cashOrCredit){
+            System.out.println("Please choose another payment option");
+        } else{
+            System.out.println("Sold. Pleasure doing business with you!");
+        }
 
-        // if payment option is NOT cash or credit: System.out.println("Please choose another payment option");
-        // otherwise: System.out.println("Sold. Pleasure doing business with you!");
 
         char letter = 'A';
-
         boolean isVowel = (letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U');
+        if(!isVowel){
+            System.out.println("The letter " + letter + " is a consonant");
+        } else{
+            System.out.println("The letter " + letter + " is a vowel");
+        }
 
-        // if letter is NOT a vowel: System.out.println("The letter " + letter + " is a consonant");
-        // otherwise: System.out.println("The letter " + letter + " is a vowel");
 
         String move = "stay";
         boolean isHitOrStay = move.equals("hit") || move.equals("stay");
-
-        // if move is NOT hit or stay: System.out.println("Please choose a valid move");
-        // otherwise: System.out.println("You win 10 bucks!");
-
+        if(!isHitOrStay){
+            System.out.println("Please choose a valid move");
+        } else{
+            System.out.println("You win 10 bucks!");
+        }
     }
 }
