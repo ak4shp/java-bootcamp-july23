959e124ee4e288fe192251c92b8235114d16d58d
Aakash Patel <akashpatelp83@gmail.com>
2023-07-22 00:30:37 +0530

    Restructure Directory

---
+++ Practice-Projects/Set 4 - Functions/Chorus.java
@@ -0,0 +1,10 @@
+public class Chorus {
+    public static void main(String[] args) {
+        // Chorus needs to be sung 5 times
+        System.out.println("Don't blame it on the sunshine");
+        System.out.println("Don't blame it on the moonlight");
+        System.out.println("Don't blame it on good times");
+        System.out.println("Don't blame it on the boogie\n");
+
+    }
+}
