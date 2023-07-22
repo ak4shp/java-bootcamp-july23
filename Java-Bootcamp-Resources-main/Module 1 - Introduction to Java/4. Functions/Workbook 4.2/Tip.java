d19a73353895bf4623eb955a2510f99210738c57
Aakash Patel <akashpatelp83@gmail.com>
2023-07-23 00:29:42 +0530

    Workbook 4.1

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/4. Functions/Workbook 4.2/Tip.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/4. Functions/Workbook 4.2/Tip.java
@@ -4,15 +4,18 @@ public class Tip {
         double bill = 53.5;
         System.out.println("Waiter: I hope you enjoyed your meal!");
         System.out.println("Thank you!");
-    
         //Task 2: Call a function that tips the waiter.
+        tipTheWaiter(bill);
     }
-
-
 
     
     //Task 1: Make a function here. See the doc comment for details.  
+    public static void tipTheWaiter(double bill){
+        double tip = bill * 0.15;  //? 15% = 0.15
+        System.out.println("Your service was wonderful! Please, accept this tip: $"+tip);
+    }
 
+    
     /**
      * Function name: tipTheWaiter - calculates a tip and prints it.
      * @param bill (double).
