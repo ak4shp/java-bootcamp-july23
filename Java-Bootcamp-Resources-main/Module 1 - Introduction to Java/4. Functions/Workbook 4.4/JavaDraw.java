53be0fb661a57258953e276863739ba632b91d2b
Aakash Patel <akashpatelp83@gmail.com>
2023-07-23 01:31:43 +0530

    Workbook 4.4

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/4. Functions/Workbook 4.4/JavaDraw.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/4. Functions/Workbook 4.4/JavaDraw.java
@@ -3,40 +3,27 @@ import java.util.Scanner;
 public class JavaDraw {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        int choice;
+			Scanner scan = new Scanner(System.in);
+			int choice;
 
-	System.out.print( "\nWhich animal would you like to draw?\n" );
-        System.out.println( "Write 1 for butterfly " );
-	System.out.println( "Write 2 for elephant  " );
-	System.out.println( "Write 3 for bear      " );
-        System.out.println( "Write 4 for snake     " );
+			System.out.print( "\nWhich animal would you like to draw?\n" );
+			System.out.println( "Write 1 for butterfly " );
+			System.out.println( "Write 2 for elephant  " );
+			System.out.println( "Write 3 for bear      " );
+			System.out.println( "Write 4 for snake     " );
 
-        //Task 1 – Pick up the user's choice.
+			//Task 1 – Pick up the user's choice.
+			choice = scan.nextInt();
 
-        //Task 3 – Call the draw function, and pass in the user's choice.
-        scan.close();
+			//Task 3 – Call the draw function, and pass in the user's choice.
+			draw(choice);
+			scan.close();
     }
 
 
-       //Task 2 – Write a function 
-
-    /**
-     * Function name: draw - draws an animal that the user chooses.
-     * @param choice: (int)
-     * 
-     * Inside the function: 
-     *  • Based on the choice, draw:
-     *      1. a butterfly
-     *      2. elephant
-     *      3. bear
-     *      4. snake
-     */  
-
-     /*
-     
-     Animal 1 
-
+    public static void draw(int choice){
+        if(choice == 1){
+        //* Animal 1
             System.out.println("  .==-.                   .-==.     ");
             System.out.println("   \\()8`-._  `.   .'  _.-'8()/     ");
             System.out.println("   (88\"   ::.  \\./  .::   \"88)     ");
@@ -49,8 +36,8 @@ public class JavaDraw {
             System.out.println("      \\O `::/       \\::' O/       ");
             System.out.println("       \"\"--'         `--\"\"      ");
 
-    Animal 2
-
+        } else if(choice == 2){
+        //* Animal 2
             System.out.println("       _..--\"\"-.                  .-\"\"--.._ ");
             System.out.println("   _.-'         \\ __...----...__ /         '-._");
             System.out.println(" .'      .:::...,'              ',...:::.      '.");
@@ -70,11 +57,8 @@ public class JavaDraw {
             System.out.println("        \\  '. `-.           /");
             System.out.println("         \\_:_)   `\"--.....-'");
 
-
-
-
-    Animal 3
-
+        } else if(choice == 3){
+        //* Animal 3
             System.out.println("            ___   .--. ");
             System.out.println("      .--.-\"   \"-' .- |");
             System.out.println("     / .-,`          .'");
@@ -97,10 +81,8 @@ public class JavaDraw {
             System.out.println("                      ``");
 
 
-
-
-    Animal 4
-
+        } else if(choice == 4){
+        //* Animal 4
             System.out.println("         ,,'6''-,.");
             System.out.println("        <====,.;;--.");
             System.out.println("        _`---===. \"\"\"==__");
@@ -117,8 +99,7 @@ public class JavaDraw {
             System.out.println("           |=|  `----\"\"\"     `\"\"\"\"\"\"\"\"         _,-'");
             System.out.println("            `=\\     __,---\"\"\"-------------\"\"\"''");
             System.out.println("                \"\"\"\"		");
-
-     
-     */
+        }
+	}
 
 }
