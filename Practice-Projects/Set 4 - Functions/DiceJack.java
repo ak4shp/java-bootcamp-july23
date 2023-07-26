ca367799281841e5aed0d0bd2196a8acbe60d48f
Aakash Patel <akashpatelp83@gmail.com>
2023-07-27 01:40:43 +0530

    Update Dice Jack

--- Practice-Projects/Set 4 - Functions/DiceJack.java
+++ Practice-Projects/Set 4 - Functions/DiceJack.java
@@ -8,11 +8,19 @@ public class DiceJack {
         int num1 = scan.nextInt();
         int num2 = scan.nextInt();
         int num3 = scan.nextInt();
-        int userSum = num1 + num2 +num3;
-
+        boolean check1 = areLessThan1(num1, num2, num3);
+        boolean check2 = areGreaterThan6(num1, num2, num3);
+        
+        if(check1 || check2){
+            System.out.println("Chosen number out of range.(Range 1-6)");
+            System.exit(0);
+        }
+        
         int dice1 = rollDice();
         int dice2 = rollDice();
         int dice3 = rollDice();
+        
+        int userSum = num1 + num2 +num3;
         int diceSum = dice1 + dice2 + dice3;
 
         int diff = userSum - diceSum;
@@ -20,10 +28,21 @@ public class DiceJack {
         if(diff > 0 && diff < 3){
             result = "You win!";
         }
+        
         System.out.println("Dice -> ("+dice1+", "+dice2+", "+dice3+")");
         System.out.println("Dice sum = "+diceSum+", Number sum = "+userSum);
         System.out.println(result);
         scan.close();
+    }
+
+
+    public static boolean areLessThan1(int num1, int num2, int num3){
+        return (num1 < 1 || num2 < 1 || num3 < 1);
+    }
+
+    
+    public static boolean areGreaterThan6(int num1, int num2, int num3){
+        return (num1 > 6 || num2 > 6 || num3 > 6);
     }
 
 
