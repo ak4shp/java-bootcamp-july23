ad229a4153092daa9466eec841f378e0abd514cb
Aakash Patel <akashpatelp83@gmail.com>
2023-07-27 23:22:15 +0530

    Dice roll (Random), Loop practice

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.10/Doubles.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.10/Doubles.java
@@ -5,11 +5,29 @@ public class Doubles {
        
         int dice1;
         int dice2;
+        int attempt=0;
+        boolean isNotDouble = true;
 
-
+        while(isNotDouble){
+            attempt++;
+            dice1 = rollDice();
+            dice2 = rollDice();
+            System.out.println("Dice 1: "+dice1);
+            System.out.println("Dice 2: "+dice2 +"\n");
+            if(dice1 == dice2){
+                System.out.println("You rolled doubles!\n(Total "+attempt+" attempts)");
+                isNotDouble = false;
+            }
+        }
     }
 
+
+    public static int rollDice(){
+        double randNum = (Math.random() * 6) + 1;
+        return (int) randNum;
+    }
 }
+
 
 
 
--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.11/JavaRolls.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.11/JavaRolls.java
@@ -4,14 +4,53 @@ public class JavaRolls {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         // See Learn the Part for detailed instructions.
-        
-        int diceRoll;
+        System.out.println( "Let's play Rolling Java. Type anything to start.");
+        String dummy = scan.nextLine();
+
+        String homeMsg ="\nGreat, here are the rules:\n\n"
+                + "\t- If you roll a 6, the game stops.\n"
+                + "\t- If you roll a 4, nothing happens.\n"
+                + "\t- Otherwise, you get 1 point.\n\n"
+                + "You must collect at least 3 points to win. Enter anything to roll:";
+
+        System.out.println(homeMsg);
+        dummy = scan.nextLine();
+
         int score = 0;
+        boolean isRoll = true;
 
+        while(isRoll){
+            int diceRoll = rollDice();
+            String rollMsg = "";
+            if(diceRoll == 6){
+                rollMsg = "End of game.";
+                isRoll = false;
+                // System.out.println("\nYou rolled a "+diceRoll+". End of game.");
+            } else if(diceRoll == 4){
+                rollMsg = "Zero point. Keep rolling";
+                // System.out.println("\nYou rolled a "+diceRoll+". Zero point. Keep rolling");
+            } else{
+                rollMsg = "One point. Keep rolling";
+                score++;
+                // System.out.println("\nYou rolled a "+diceRoll+". One point. Keep rolling");
+            }
+            System.out.println("\nYou rolled a "+diceRoll+". "+rollMsg);
+        }
+
+        String endMsg = "";
+        if(score >=3){
+            endMsg = "Wow, that's lucky. You win!";
+        } else{endMsg = "Tough luck, you lose :(";}
+        
+        System.out.println("\nYour score is "+score);
+        System.out.println(endMsg);
+
+        scan.close();
     }
-  
-  
+    
 
-  
-  
+    public static int rollDice(){
+        double randNum = (Math.random() * 6) + 1;
+        return (int) randNum;
+    }
 }
